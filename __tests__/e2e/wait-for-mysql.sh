ecc7636d6bc69259f16a1c3b3fb0b12e3a46557d
dkress59 <28515387+dkress59@users.noreply.github.com>
2021-11-28 13:36:00 +0100

    try

--- __tests__/e2e/wait-for-mysql.sh
+++ __tests__/e2e/wait-for-mysql.sh
@@ -6,7 +6,7 @@ PASSWORD="wordpress_db_password"
 
 sleep 5
 while ! docker exec $CONTAINER mysql --user=$USERNAME --password=$PASSWORD -e "SELECT 1" >/dev/null 2>&1; do
-    echo "\ntesting connection..." && sleep 1
+    echo "testing connection..." && sleep 1
 done
 
-docker exec $CONTAINER mysql --user=$USERNAME --password=$PASSWORD -e "SELECT * from wordpress"
+docker exec $CONTAINER mysql --user=$USERNAME --password=$PASSWORD -e "SELECT wordpress.*"
