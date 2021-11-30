232ebcdc864b68b87760df7a2628544978d30204
dkress59 <28515387+dkress59@users.noreply.github.com>
2021-11-30 11:07:32 +0100

    try

--- __tests__/e2e/wait-for-db-import.sh
+++ __tests__/e2e/wait-for-db-import.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 CONTAINER="e2e_db_1"
+USER="wordpress_db_user"
+PASS="wordpress_db_password"
+DB="wordpress"
 
 sleep 3
 while ! docker exec $CONTAINER mysqladmin ping --password=password; do
     echo "testing connection..." && mysqladmin ping --password=password && sleep 1
 done
-sleep 3
+
+docker cp __tests__/e2e/db.sql $CONTAINER:/
+docker exec $CONTAINER /bin/sh -c "mysql -u$USER -p$PASS $DB < /db.sql"
