6615d2f8d9ccb07d61061823d1fc127fae910378
dkress59 <28515387+dkress59@users.noreply.github.com>
2021-11-30 13:25:20 +0100

    wait quietly

--- __tests__/e2e/wait-for-db-import.sh
+++ __tests__/e2e/wait-for-db-import.sh
@@ -4,7 +4,7 @@ CONTAINER="e2e_db_1"
 DB="wordpress"
 
 sleep 3
-while ! docker exec $CONTAINER /bin/sh -c "mysqladmin --protocol=tcp ping -ppassword>/dev/null"; do
+while ! docker exec $CONTAINER /bin/sh -c "mysqladmin --protocol=tcp ping -ppassword >/dev/null 2>&1"; do
     echo "testing connection..."
     sleep 1
 done
